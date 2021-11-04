60e780a2fdf9eff2a95a82e8861500f9813e3470
Nichibotsu <nbesecke05@gmail.com>
2021-11-04 18:08:43 +0100

    Projektdateien hinzufügen.

---
+++ Inzidenz/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Inzidenz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 18);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Sachsen-Anhalt",
+            "Sachsen",
+            "Thüringen"});
+            this.comboBox1.Location = new System.Drawing.Point(117, 32);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(180, 28);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Auswählen";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bundesland:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(117, 72);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 28);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 75);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Infizierte:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 34);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Inzidenz:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 71);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Flächenfaktor:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(140, 18);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 35);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(18, 71);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(394, 117);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Eingabe";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(18, 216);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(394, 110);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ausgabe";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 365);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Inzidenz";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Inzidenz/Form1.cs
@@ -0,0 +1,94 @@
+﻿//Niclas Besecke
+//11.2
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inzidenz
+{
+    public partial class Form1 : Form
+    {
+        class Bundesland
+        {
+            public double einwohner;
+            public double Fläche;
+            public double infizierte;
+
+            public static double Rechner(Bundesland Ausgabe)//Inzidenz
+            {
+                return Math.Round((Ausgabe.infizierte * 100000) / Ausgabe.einwohner, 2);
+            }
+            public  static double Flaeche(Bundesland Ausgabe)//Flächenfaktor
+            {
+                return Math.Round(Ausgabe.infizierte / Ausgabe.Fläche, 2);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double Inzidenz,Flächenfaktor;
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Infizierte eingeben");
+            }
+            else if (comboBox1.Text=="Auswählen")
+            {
+                MessageBox.Show("Bundesland auswählen");
+            }
+            else
+            {
+                Bundesland SachsenAnhalt = new Bundesland();
+                Bundesland Sachsen = new Bundesland();
+                Bundesland Thüringen = new Bundesland();
+
+                if (comboBox1.Text == "Sachsen-Anahlt")
+                {
+                    SachsenAnhalt.einwohner = 2180684;
+                    SachsenAnhalt.Fläche = 20456.51;
+                    SachsenAnhalt.infizierte = Convert.ToDouble(textBox1.Text);
+                    Inzidenz = Bundesland.Rechner(SachsenAnhalt);
+                    Flächenfaktor = Bundesland.Flaeche(SachsenAnhalt);
+                    label3.Text = "Inzidenz:" + Convert.ToString(Inzidenz);
+                    label4.Text = "Flächenfaktor:" + Convert.ToString(Flächenfaktor) + " km²";
+                }
+                else if (comboBox1.Text == "Sachsen")
+                {
+                    Sachsen.einwohner = 4056941;
+                    Sachsen.Fläche=18449.93;
+                    Sachsen.infizierte = Convert.ToDouble(textBox1.Text);
+                    Inzidenz = Bundesland.Rechner(Sachsen);
+                    Flächenfaktor = Bundesland.Flaeche(Sachsen);
+                    label3.Text = "Inzidenz:" + Convert.ToString(Inzidenz);
+                    label4.Text = "Flächenfaktor:" + Convert.ToString(Flächenfaktor) + " km²";
+                }
+                else
+                {
+                    Thüringen.einwohner = 2120237;
+                    Thüringen.Fläche = 16202.35;
+                    Thüringen.infizierte = Convert.ToDouble(textBox1.Text);
+                    Inzidenz= Bundesland.Rechner(Thüringen);
+                    Flächenfaktor=Bundesland.Flaeche(Thüringen);
+                    label3.Text = "Inzidenz:"+Convert.ToString(Inzidenz);
+                    label4.Text = "Flächenfaktor:"+Convert.ToString(Flächenfaktor)+" km²";
+
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
